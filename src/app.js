f8ab9b8090c81d79a41faf6d4d39c127fca61a23
10x0 <aasseess@pm.me>
2025-04-16 16:35:27 +0100

    fix: CORS error

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow all common HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow common headers
+}));
 
 export { app };
